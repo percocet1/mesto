5c25e45052e43ec06b90a05355000ecbcc5c234b
Dmitriy Minaev <ApexGlide1@yandex.kz>
2023-04-20 07:39:34 +0300

    fix:adaptive issue

--- script/script.js
+++ script/script.js
@@ -3,23 +3,24 @@ let profileEditButton = content.querySelector('.profile__edit-button')
 let closePopup = document.querySelector('.popup__close-button')
 
 let editForm = document.querySelector('.popup__edit-profile-form')
-let formItem = document.querySelector('.popup__form-item')
 let username = content.querySelector('.profile__username')
 let undertitle = content.querySelector('.profile__undertitle')
-let nameInput = editForm.querySelector('.popup__form-item_name')
-let jobInput =  editForm.querySelector('.popup__form-item_job')
+let nameInput = editForm.querySelector('.popup__form-item-name')
+let jobInput =  editForm.querySelector('.popup__form-item-job')
+let buttonSubmit = document.querySelector('.popup__button-submit')
 
 profileEditButton.addEventListener('click', function(e){
-    e.preventDefault();
     popup.classList.add('popup_opened');
     nameInput.value = username.textContent
     jobInput.value = undertitle.textContent
-    editForm.addEventListener('submit', function handleFormSubmit (evt) {
-        evt.preventDefault();
-        username.textContent = nameInput.value;
-        undertitle.textContent = jobInput.value;
-    })    
 })
+
+editForm.addEventListener('submit', function handleFormSubmit (evt) {
+    evt.preventDefault();
+    username.textContent = nameInput.value;
+    undertitle.textContent = jobInput.value;
+    buttonSubmit.addEventListener('click', ()=> {popup.classList.remove('popup_opened');})
+})    
 
 closePopup.addEventListener('click', () => {
     popup.classList.remove('popup_opened');
