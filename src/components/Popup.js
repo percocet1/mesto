6277a322dbabb9cb6cb00c30e0452271f24266ed
Dmitriy Minaev <ApexGlide1@yandex.kz>
2023-10-16 18:38:37 +0300

    hotfix: 1.01

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,21 +1,17 @@
 export default class Popup{
     constructor(popupSelector){
         this._popup = document.querySelector(popupSelector)
-        this._buttonClose = this._popup.querySelector('.popup__close-button')
+        this._handleEscClose = this._handleEscClose.bind(this)
     }
 
     open(){
         this._popup.classList.add('popup_opened')
-        document.addEventListener('keydown', (evt)=>{
-            this._handleEscClose(evt)
-        })
+        document.addEventListener('keydown', this._handleEscClose)
     }
 
     close(){
         this._popup.classList.remove('popup_opened')
-        document.removeEventListener('keydown', (evt)=>{
-            this._handleEscClose(evt)
-        })
+        document.removeEventListener('keydown', this._handleEscClose)
     }
     
     _handleEscClose(evt){
@@ -31,11 +27,10 @@ export default class Popup{
     }
 
     setEventListeners(){
-        this._buttonClose.addEventListener('click', ()=>{
-            this.close()
-        })
         this._popup.addEventListener('click', (evt)=>{
-            this._handleClickOnOverlayClose(evt)
+            if (evt.target.classList.contains('popup_opened') || evt.target.classList.contains('popup__close-button')) {
+                this.close();
+              }
         })
     }
 }
